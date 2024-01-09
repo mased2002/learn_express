9055ed6b07ab1b81fa7c565346661aa71517857b
mased2002 <edsam2302@gmail.com>
2024-01-09 19:52:10 +0200

    add the get all posts

--- routes.js
+++ routes.js
@@ -3,6 +3,11 @@ const express = require("express")
     const router = express.Router()
     
     // Get all posts
+ router.get("/posts", async (req, res) => {
+        const posts = await Post.find()
+        res.send(posts)
+    })		
+
     router.post("/posts", async (req, res) => {
         const post = new Post({
             title:req.body.title,
